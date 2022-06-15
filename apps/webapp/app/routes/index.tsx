bcd710991418b79b16e5e3cfbba815b279354783
Elad Shaham <7040645+eshaham@users.noreply.github.com>
2022-06-15 05:47:46 +0300

    integrate mantine

--- apps/webapp/app/routes/index.tsx
+++ apps/webapp/app/routes/index.tsx
@@ -1,7 +1,10 @@
+import { Button } from '@mantine/core';
+
 export default function Index() {
   return (
     <div style={{ fontFamily: 'system-ui, sans-serif', lineHeight: '1.4' }}>
       <h1>Welcome to Remix</h1>
+      <Button>Hello world!</Button>
       <ul>
         <li>
           <a
